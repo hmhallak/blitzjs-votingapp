f13aa9edffd3a358f4dd53346be0a1ab70167661
Hussein Mohamad Hallak <hu100wtf@gmail.com>
2022-04-01 22:37:03 -0400

    update app name

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -1,10 +1,10 @@
 import { Head, BlitzLayout } from "blitz"
 
-const Layout: BlitzLayout<{title?: string}> = ({ title, children }) => {
+const Layout: BlitzLayout<{ title?: string }> = ({ title, children }) => {
   return (
     <>
       <Head>
-        <title>{title || "todolist"}</title>
+        <title>{title || "votingapp"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
